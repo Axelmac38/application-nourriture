ca48cc2d7fa426761868408c7c068faccd284e1b
Axelmac38 <axelmac@hotmail.fr>
2026-09-20 14:45:04 +0200

    Synchroniser les unites des lignes de courses

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'repas-stock-v101';
+const CACHE = 'repas-stock-v102';
 const ASSETS = ['./', './index.html', './styles.css', './recipe.css', './src/main.js', './src/domain.js', './manifest.webmanifest', './assets/icon.svg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
@@ -15,6 +15,7 @@ self.addEventListener('fetch', (event) => {
   }
   event.respondWith(caches.match(request).then((cached) => cached || fetch(request)));
 });
+
 
 
 
